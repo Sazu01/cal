d22967ab1aaff0b810964b59d6e977cfeffd4757
Sazu01 <sazuislam15@gmail.com>
2022-03-09 22:10:49 +0600

    OK: Numbers, BS, AC

--- app.js
+++ app.js
@@ -30,47 +30,104 @@ const btn_0 = document.querySelector("div.number.fx.number0")
 const btn_Icon = document.querySelector("div.number.fx.Icon")
 
 
+
+let temp = ""
+
+
 btn_wrapper.addEventListener("click", e => {
-    const inputs = new Array();
+
 
     if(e.target === btn_AC){
-        // AC
         console.log(`btn_AC`)
-    }else if (e.target === btn_Per){
-        console.log(`btn_Per`)
-    }else if (e.target === btn_D){
-        console.log(`btn_D`)
-    }else if (e.target === btn_x){
-        console.log(`btn_x`)
-    }else if (e.target === btn_7){
-        console.log(`btn_7`)
-    }else if (e.target === btn_8){
-        console.log(`btn_8`)
-    }else if (e.target === btn_9){
-        console.log(`btn_9`)
-    }else if (e.target === btn_M){
-        console.log(`btn_M`)
-    }else if (e.target === btn_4){
-        console.log(`btn_4`)
-    }else if (e.target === btn_5){
-        console.log(`btn_5`)
-    }else if (e.target === btn_6){
-        console.log(`btn_6`)
-    }else if (e.target === btn_Plus){
-        console.log(`btn_Plus`)
-    }else if (e.target === btn_1){
-        console.log(`btn_1`)
-    }else if (e.target === btn_2){
-        console.log(`btn_2`)
-    }else if (e.target === btn_3){
-        console.log(`btn_3`)
-    }else if (e.target === btn_E){
-        console.log(`btn_E`)
-    }else if (e.target === btn_Dot){
-        console.log(`btn_Dot`)
-    }else if (e.target === btn_0){
-        console.log(`btn_0`)
-    }else if (e.target === btn_Icon){
-        console.log(`btn_Icon`)
+        temp = ""
     }
+
+    else if (e.target === btn_Icon){
+        // back btn
+        console.log(`btn_Icon`)
+        temp = temp.slice(0, -1)
+    }
+
+    else if (e.target === btn_Per){
+        console.log(`btn_Per`)
+    }
+
+    else if (e.target === btn_M){
+        console.log(`btn_M`)
+    }
+
+    else if (e.target === btn_Plus){
+        console.log(`btn_Plus`)
+    }
+
+    else if (e.target === btn_x){
+        console.log(`btn_x`)
+    }
+
+    else if (e.target === btn_D){
+        console.log(`btn_D`)
+    }
+
+    else if (e.target === btn_E){
+        console.log(`btn_E`)
+    }
+
+    // Numbers
+    else if (e.target === btn_1){
+        console.log(`btn_1`)
+        temp += "1"
+    }
+
+    else if (e.target === btn_2){
+        console.log(`btn_2`)
+        temp += "2"
+    }
+
+    else if (e.target === btn_3){
+        console.log(`btn_3`)
+        temp += "3"
+    }
+
+    else if (e.target === btn_4){
+        console.log(`btn_4`)
+        temp += "4"
+    }
+
+    else if (e.target === btn_5){
+        console.log(`btn_5`)
+        temp += "5"
+    }
+
+    else if (e.target === btn_6){
+        console.log(`btn_6`)
+        temp += "6"
+    }
+
+    else if (e.target === btn_7){
+        console.log(`btn_7`)
+        temp += "7"
+    }
+
+    else if (e.target === btn_8){
+        console.log(`btn_8`)
+        temp += "8"
+    }
+
+    else if (e.target === btn_9){
+        console.log(`btn_9`)
+        temp += "9"
+    }
+
+    else if (e.target === btn_0){
+        console.log(`btn_0`)
+        temp += "0"
+    }
+    
+    else if (e.target === btn_Dot){
+        console.log(`btn_Dot`)
+        temp += "."
+    }
+
+    result.innerHTML = `${temp}`
+    console.log(temp);
 })
