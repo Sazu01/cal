8400ee38c11a0e2ac392442a380862c735839a76
Sazu01 <sazuislam15@gmail.com>
2022-03-10 16:04:40 +0600

    Basics Fixed

--- app.js
+++ app.js
@@ -22,24 +22,23 @@ const btn_Icon = document.querySelector("div.number.fx.Icon")
 
 function calculate(input) {
 
-    var f = {
-        add: '+'
-        , sub: '-'
-        , div: '/'
-        , mlt: '*'
-        , mod: '%'
-        , exp: '^'
+    const f = {
+        add: '+',
+        sub: '-',
+        div: '/',
+        mlt: '*',
+        mod: '%',
+        exp: '^'
     };
 
-    f.ooo = [[[f.mlt], [f.div], [f.mod], [f.exp]],
-    [[f.add], [f.sub]]];
+    f.ooo = [[[f.mlt], [f.div], [f.mod], [f.exp]], [[f.add], [f.sub]]];
 
     input = input.replace(/[^0-9%^*\/()\-+.]/g, '');
 
-    var output;
-    for (var i = 0, n = f.ooo.length; i < n; i++) {
+    let output;
+    for (let i = 0, n = f.ooo.length; i < n; i++) {
 
-        var re = new RegExp('(\\d+\\.?\\d*)([\\' + f.ooo[i].join('\\') + '])(\\d+\\.?\\d*)');
+        let re = new RegExp('(\\d+\\.?\\d*)([\\' + f.ooo[i].join('\\') + '])(\\d+\\.?\\d*)');
         re.lastIndex = 0; 
 
         while (re.test(input)) {
